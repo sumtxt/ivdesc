348dc171fa2dc5f6b3ae64f81e0ec579124edfd0
MM <none@none.com>
2020-01-01 13:17:34 +0100

    Analytical SE: Changed definition of cell Cov(3,3)

--- R/ivdesc/R/utils.R
+++ R/ivdesc/R/utils.R
@@ -3,7 +3,7 @@ get_var_mu_co <- function(N,mu,mu_nt,mu_at,v,v_nt,v_at,pz,pi_nt,pi_at,pi_co){
   # Covariance matrix
   Cov <- matrix(c(v/N, v_nt/N,v_at/N,(mu_nt-mu)*pi_nt/N,(mu_at-mu)*pi_at/N,
                   v_nt/N,v_nt/(pz*pi_nt*N),0,0,0,
-                  v_at/N,0,v_at/(pz*pi_at*N),0,0,
+                  v_at/N,0,v_at/( (1-pz)*pi_at*N),0,0,
                   (mu_nt-mu)*pi_nt/N,0,0,pi_nt*(1-pi_nt)/(pz*N),0,
                   (mu_at-mu)*pi_at/N,0,0,0,pi_at*(1-pi_at)/((1-pz)*N)),nrow=5)
 
